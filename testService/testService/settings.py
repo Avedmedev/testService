f15b53ff15d3a6529d8f1236647d127670704f2a
Alexey Vedmedev <al.vedmedev@gmail.com>
2022-06-26 15:08:19 +0300

    remove secret key

--- testService/testService/settings.py
+++ testService/testService/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-import testService.settings_local
+#import testService.settings_local
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hcrjl+hf1qd&9i@m#!k$)f4u&m^cf*e26a8kimc(*1(c_jno!p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
